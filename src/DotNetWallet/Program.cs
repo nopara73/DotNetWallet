87c575a83ed7b8bd976f52ce3244ffb5aa80ae7f
nopara73 <adam.ficsor73@gmail.com>
2016-11-29 05:30:34 +0800

    Parsing command line arguments

--- src/DotNetWallet/Program.cs
+++ src/DotNetWallet/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using NBitcoin;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -26,7 +28,7 @@ namespace DotNetWallet
 
 		public static void Main(string[] args)
 		{
-			args = new string[] { "generate-wallet", "a", "w"};
+			args = new string[] { "send", "btc=3.2", "address=1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX" };
 
 			if (args.Length == 0)
 			{
@@ -50,64 +52,111 @@ namespace DotNetWallet
 			#endregion
 
 			var walletFileName = DefaultWalletFileName;
+
 			#region GenerateWalletCommand
 			if (command == "generate-wallet")
 			{
 				AssertArgumentsLenght(args.Length, 1, 2);
-				if(args.Length > 1)
-				{
-					if (!IsCorrectWalletFileName(args[1]))
-					{
-						WriteLine("Second argument is not correct wallet file name.");
-						WriteLine("Correct wallet file name ends with .json");
-						WriteLine($"If this argument is not specified the default wallet will be used: {DefaultWalletFileName}");
-						Exit();
-					}
-					walletFileName = args[1];
-				}
+				walletFileName = GetWalletFileName(args);
+
 			}
 			#endregion
 			#region ShowBalancesCommand
 			if (command == "show-balances")
 			{
 				AssertArgumentsLenght(args.Length, 1, 2);
-				if (args.Length > 1)
-				{
-					if (IsCorrectWalletFileName(args[1]))
-						AssertWalletFormat(args[1]);
-				}
-				walletFileName = args[1];
+				walletFileName = GetWalletFileName(args);
+				AssertCorrectWalletFormat(walletFileName);
 			}
 			#endregion
 			#region ShowHistoryCommand
 			if (command == "show-history")
 			{
 				AssertArgumentsLenght(args.Length, 1, 2);
+				walletFileName = GetWalletFileName(args);
+				AssertCorrectWalletFormat(walletFileName);
 			}
 			#endregion
 			#region ReceiveCommand
 			if (command == "receive")
 			{
 				AssertArgumentsLenght(args.Length, 1, 2);
+				walletFileName = GetWalletFileName(args);
+				AssertCorrectWalletFormat(walletFileName);
 			}
 			#endregion
 			#region SendCommand
 			if (command == "send")
 			{
+				//AssertArgumentsLenght(args.Length, 1, 2);
+				walletFileName = GetWalletFileName(args);
+				AssertCorrectWalletFormat(walletFileName);
 
+				try
+				{
+					var amountToSend = new Money(GetAmountToSend(args), MoneyUnit.BTC);
+
+					//todo check for expected network
+					var addressToSend = BitcoinAddress.Create(GetAddressToSend(args));
+				}
+				catch(Exception ex)
+				{
+					WriteLine(ex);
+					Exit();
+				}
 			}
 			#endregion
 
 			Exit();
 		}
-		public static void AssertWalletFormat(string walletFileName)
+
+		public static void AssertCorrectWalletFormat(string walletFileName)
 		{
+			//todo
+			throw new NotImplementedException();
 		}
-		public static bool IsCorrectWalletFileName(string walletFileName)
+		private static string GetAddressToSend(string[] args)
 		{
-			if (walletFileName.EndsWith(".json", StringComparison.OrdinalIgnoreCase))
-				return true;
-			return false;
+			string address = "";
+			foreach (var arg in args)
+				if (arg.StartsWith("address=", StringComparison.OrdinalIgnoreCase))
+					address = arg.Substring(arg.IndexOf("=") + 1);
+			if (address == "")
+			{
+				WriteLine(@"'address=' is not specified.");
+				Exit();
+			}
+			return address;
+		}
+		private static decimal GetAmountToSend(string[] args)
+		{
+			decimal amount = -1m;
+			foreach (var arg in args)
+				if (arg.StartsWith("btc=", StringComparison.OrdinalIgnoreCase))
+					if (!decimal.TryParse(
+						arg.Substring(arg.IndexOf("=") + 1).Replace(',', '.'),
+						NumberStyles.Any,
+						CultureInfo.InvariantCulture,
+						out amount))
+					{
+						WriteLine("Wrong btc amount format.");
+						Exit();
+					}
+			if(amount == -1m)
+			{
+				WriteLine(@"'btc=' is not specified.");
+				Exit();
+			}
+			return amount;
+		}
+		private static string GetWalletFileName(string[] args)
+		{
+			string walletFileName = DefaultWalletFileName;
+
+			foreach (var arg in args)
+				if (arg.StartsWith("wallet-file=", StringComparison.OrdinalIgnoreCase))
+					walletFileName = arg.Substring(arg.IndexOf("=") + 1);
+			return walletFileName;
 		}
 		// Inclusive
 		public static void AssertArgumentsLenght(int length, int min, int max)
@@ -123,19 +172,16 @@ namespace DotNetWallet
 				Exit();
 			}
 		}
-
 		public static void DisplayHelp()
 		{
 			WriteLine("Possible commands are:");
 			foreach (var cmd in Commands) WriteLine($"\t{cmd}");
 		}
-
 		public static void Exit()
 		{
 			WriteLine("Press Enter to exit...");
 			ReadLine();
 			Environment.Exit(0);
 		}
-
 	}
 }
